dd45bf5a34ea965d051213992ba6dea5eddfb77a
kiyomizuhi <hiroyukiinoue1444@gmail.com>
2021-04-18 23:22:09 +0900

    fix

--- src/power_consumption/get_data.sh
+++ src/power_consumption/get_data.sh
@@ -2,4 +2,5 @@
 
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip
 mv household_power_consumption.zip data/raw
-unzip data/raw/household_power_consumption.zip data/raw/
+unzip data/raw/household_power_consumption.zip
+mv household_power_consumption.txt data/raw
